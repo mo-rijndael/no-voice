5ba2ffe93f235b1e586c9b51cfa5e31114fe468f
Termonoid <termonoid@yandex.ru>
2020-05-31 20:20:25 +0300

    migrated to ureq instead reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use reqwest;
 use serde::Deserialize;
-use std::collections::HashMap;
 use rand::Rng;
 use rand::seq::SliceRandom;
 
 use std::fs::OpenOptions;
 use std::io::Read;
 
-const TOKEN: &'static str = env!("TOKEN");
-const GID:   &'static str = env!("GROUP_ID");
+const TOKEN: &str = env!("TOKEN");
+const GID:   &str = env!("GROUP_ID");
 
 const SEND:   &str = "https://api.vk.com/method/messages.send";
 const GET_LP: &str = "https://api.vk.com/method/groups.getLongPollServer";
@@ -48,7 +46,6 @@ struct ResponseVK{
     response: LongPoll
 }
 fn main() {
-    let client = reqwest::blocking::Client::new();
     let (mut key, server, mut  ts) = get_longpoll();
     let mut rng = rand::thread_rng();
 
@@ -67,17 +64,22 @@ fn main() {
         
     };
     loop {
-        let mut updates_request = 
+        let updates_request = 
         {
-            let mut params = HashMap::new();
+            /*let mut params = HashMap::new();
                 params.insert("act","a_check");
                 params.insert("key",&key);
                 params.insert("ts", &ts);
-                params.insert("wait", "25");
-
-            match client.post(&server)
-                        .form(&params)
-                        .send(){
+                params.insert("wait", "25");*/
+            let params = [
+                ("act","a_check"),
+                ("key",&key),
+                ("ts", &ts),
+                ("wait", "25")
+            ];
+            match ureq::post(&server)
+                        .send_form(&params)
+                        .into_string(){
                 Ok(some) => some,
                 Err(e) => {
                     eprintln!("Error while getting updates: {}",e);
@@ -86,7 +88,7 @@ fn main() {
             }
         };
         let longpoll_r: 
-            Result<LongPollResponse, _> = serde_json::from_reader(&mut updates_request);
+            Result<LongPollResponse, _> = serde_json::from_str(&updates_request);
         let updates;
         match longpoll_r {
             Ok(u) => {
@@ -96,7 +98,7 @@ fn main() {
 
             Err(_) => {
                 let longpoll_err: 
-                    LongPollError = serde_json::from_reader(&mut updates_request).unwrap();
+                    LongPollError = serde_json::from_str(&updates_request).unwrap();
 
                     if longpoll_err.failed == 1 {
                          ts=longpoll_err.ts.unwrap(); 
@@ -124,18 +126,24 @@ fn main() {
                         let peer_id = message.peer_id.to_string();
                         println!("sending \"{}\" to {}", text, peer_id);
 
-                        let mut params = HashMap::new();
-                        params.insert("access_token", TOKEN);
-                        params.insert("random_id",&random_id);
-                        params.insert("message", &text);
-                        params.insert("peer_id",&peer_id);
-                        params.insert("v", "5.95");
-                        match client.post(SEND)
-                                    .form(&params)
-                                    .send(){
-                                    Ok(_) => {},
-                                    Err(e) => eprintln!("Sending error: {}", e)
-                                    }
+                        /*let mut params = HashMap::new();
+                        params.insert("access_token", TOKEN);           
+                        params.insert("random_id",&random_id);          
+                        params.insert("message", &text);            
+                        params.insert("peer_id",&peer_id);          
+                        params.insert("v", "5.95");*/             
+                        let params = [
+                            ("access_token", TOKEN),
+                            ("random_id",&random_id),
+                            ("message", &text),
+                            ("peer_id",&peer_id),
+                            ("v", "5.95")
+                        ];
+                        if ureq::post(SEND)
+                                    .send_form(&params)
+                                    .error(){
+                                        eprintln!("Sending error");
+                        }
                         
                         
                     
@@ -146,17 +154,21 @@ fn main() {
 
 }
 fn get_longpoll() -> (String, String, String){
-    let client = reqwest::blocking::Client::new();
     
-    let mut params = HashMap::new();
-    params.insert("group_id",GID);
+    /*let mut params = HashMap::new();
+    params.insert("group_id",GID);          
     params.insert("access_token", TOKEN);
-    params.insert("v", "5.95");
-    let response = client.post(GET_LP)
-        .form(&params)
-        .send()
+    params.insert("v", "5.95");*/           
+    let params = [
+        ("group_id",GID), 
+        ("access_token", TOKEN),
+        ("v", "5.95")
+    ];
+    let response = ureq::post(GET_LP)
+        .send_form(&params)
+        .into_string()
         .unwrap();
-    let params:ResponseVK = serde_json::from_str(&response.text().unwrap()).unwrap();
+    let params:ResponseVK = serde_json::from_str(&response).unwrap();
     let lp = params.response;
     (lp.key, lp.server, lp.ts)
 }
