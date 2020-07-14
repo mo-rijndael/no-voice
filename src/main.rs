adb448de954598c4391921b45fa28536639a4eec
Termonoid <termonoid@yandex.ru>
2020-07-14 15:19:24 +0300

    total refactoring

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rand::seq::SliceRandom;
 
 use std::fs::OpenOptions;
 use std::io::Read;
+use std::collections::VecDeque;
 
 const TOKEN: &str = env!("TOKEN");
 const GID:   &str = env!("GROUP_ID");
@@ -26,15 +27,82 @@ struct Update{
     object: Message
 }
 #[derive(Deserialize)]
-struct LongPollResponse{
-    ts: String,
-    updates: Vec<Update>
-}
-#[derive(Deserialize)]
 struct LongPoll{
     key: String,
     server: String,
-    ts: String
+    ts: String,
+    #[serde(skip)]
+    cache: VecDeque<Message>,
+}
+impl LongPoll {
+    fn new() -> Self {
+        let params = [
+            ("group_id",GID), 
+            ("access_token", TOKEN),
+            ("v", "5.95")
+        ];
+        let response = ureq::post(GET_LP)
+            .send_form(&params)
+            .into_string()
+            .unwrap();
+        let params:ResponseVK = serde_json::from_str(&response).unwrap();
+        params.response
+    }
+    fn get_events(&mut self) {
+        let updates_request = 
+        {
+            let params = [
+                ("act","a_check"),
+                ("key",&self.key),
+                ("ts", &self.ts),
+                ("wait", "25")
+            ];
+            match ureq::post(&self.server)
+                        .send_form(&params)
+                        .into_string(){
+                Ok(some) => some,
+                Err(e) => {
+                    eprintln!("Error while getting updates: {}",e);
+                    return
+                }
+            }
+        };
+        let longpoll_r: LongPollResponse = serde_json::from_str(&updates_request).unwrap();
+        match longpoll_r {
+            LongPollResponse::Normal(u) => {
+                self.ts=u.ts;
+                self.cache.extend(u.updates.into_iter().map(|x|x.object))
+            }
+
+            LongPollResponse::Failed(e) => {
+                eprintln!("got LongPoll error. Code: {}", e.failed);
+                let new_longpoll = Self::new();
+                match e.failed {
+                    1 => {self.ts = e.ts.unwrap()}
+                    2 => {self.key = new_longpoll.key}
+                    3 => {self.key = new_longpoll.key;
+                          self.ts = new_longpoll.ts}
+                    _ => {}
+                    
+                }
+                return
+            }
+        };
+    }
+}
+impl std::iter::Iterator for LongPoll {
+    type Item = Message;
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.cache.is_empty() {
+            self.get_events()
+        }
+        self.cache.pop_front()
+    }
+}
+#[derive(Deserialize)]
+struct LongPollOk{
+    ts: String,
+    updates: Vec<Update>
 }
 #[derive(Deserialize)]
 struct LongPollError{
@@ -42,11 +110,17 @@ struct LongPollError{
     ts: Option<String>
 }
 #[derive(Deserialize)]
+#[serde(untagged)]
+enum LongPollResponse {
+    Normal(LongPollOk),
+    Failed(LongPollError),
+}
+#[derive(Deserialize)]
 struct ResponseVK{
     response: LongPoll
 }
 fn main() {
-    let (mut key, server, mut  ts) = get_longpoll();
+    let long_poll = LongPoll::new();
     let mut rng = rand::thread_rng();
 
     let phrases: Vec<String> = {
@@ -56,119 +130,35 @@ fn main() {
             .unwrap();
         let mut text = String::new();
         file.read_to_string(&mut text).unwrap();
-        let mut texts: Vec<String> = Vec::new();
-        for i in text.split('\n'){
-            texts.push(String::from(i))
-        }
-        texts
-        
+        text.split(' ')
+            .map(String::from)
+            .collect()
     };
-    loop {
-        let updates_request = 
-        {
-            /*let mut params = HashMap::new();
-                params.insert("act","a_check");
-                params.insert("key",&key);
-                params.insert("ts", &ts);
-                params.insert("wait", "25");*/
-            let params = [
-                ("act","a_check"),
-                ("key",&key),
-                ("ts", &ts),
-                ("wait", "25")
-            ];
-            match ureq::post(&server)
-                        .send_form(&params)
-                        .into_string(){
-                Ok(some) => some,
-                Err(e) => {
-                    eprintln!("Error while getting updates: {}",e);
-                    continue
-                }
-            }
-        };
-        let longpoll_r: 
-            Result<LongPollResponse, _> = serde_json::from_str(&updates_request);
-        let updates;
-        match longpoll_r {
-            Ok(u) => {
-                updates=u.updates;
-                ts=u.ts;
-            }
-
-            Err(_) => {
-                let longpoll_err: 
-                    LongPollError = serde_json::from_str(&updates_request).unwrap();
-
-                    if longpoll_err.failed == 1 {
-                         ts=longpoll_err.ts.unwrap(); 
-                         continue
+    for message in long_poll {
+        if !message.attachments.is_empty()
+        && message.attachments[0].type_ == "audio_message"{
+                {
+                    let random_id = rng.gen::<i64>().to_string();
+                    let text = phrases.choose(&mut rng).unwrap();
+                    let peer_id = message.peer_id.to_string();
+                    println!("sending \"{}\" to {}", text, peer_id);
+                    let params = [
+                        ("access_token", TOKEN),
+                        ("random_id",&random_id),
+                        ("message", &text),
+                        ("peer_id",&peer_id),
+                        ("v", "5.95")
+                    ];
+                    if ureq::post(SEND)
+                                .send_form(&params)
+                                .error(){
+                                    eprintln!("Sending error");
                     }
-                    let p=get_longpoll();
-                    if longpoll_err.failed == 2 {
-                        key=p.0
-                    }
-                    else if longpoll_err.failed == 3 {
-                        key=p.0;
-                        ts=p.2;
-                    }
-                eprintln!("got LongPoll error. Code: {}", longpoll_err.failed);
-                continue;
-            }
-        }
-        for u in updates {
-            let message = u.object;
-            if !message.attachments.is_empty()
-                && message.attachments[0].type_ == "audio_message"{
-                    {
-                        let random_id = rng.gen::<i64>().to_string();
-                        let text = phrases.choose(&mut rng).unwrap();
-                        let peer_id = message.peer_id.to_string();
-                        println!("sending \"{}\" to {}", text, peer_id);
-
-                        /*let mut params = HashMap::new();
-                        params.insert("access_token", TOKEN);           
-                        params.insert("random_id",&random_id);          
-                        params.insert("message", &text);            
-                        params.insert("peer_id",&peer_id);          
-                        params.insert("v", "5.95");*/             
-                        let params = [
-                            ("access_token", TOKEN),
-                            ("random_id",&random_id),
-                            ("message", &text),
-                            ("peer_id",&peer_id),
-                            ("v", "5.95")
-                        ];
-                        if ureq::post(SEND)
-                                    .send_form(&params)
-                                    .error(){
-                                        eprintln!("Sending error");
-                        }
-                        
-                        
                     
-                }
+                    
+                
             }
         }
     }
 
-}
-fn get_longpoll() -> (String, String, String){
-    
-    /*let mut params = HashMap::new();
-    params.insert("group_id",GID);          
-    params.insert("access_token", TOKEN);
-    params.insert("v", "5.95");*/           
-    let params = [
-        ("group_id",GID), 
-        ("access_token", TOKEN),
-        ("v", "5.95")
-    ];
-    let response = ureq::post(GET_LP)
-        .send_form(&params)
-        .into_string()
-        .unwrap();
-    let params:ResponseVK = serde_json::from_str(&response).unwrap();
-    let lp = params.response;
-    (lp.key, lp.server, lp.ts)
 }
